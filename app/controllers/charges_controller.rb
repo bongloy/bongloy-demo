07ec9c18cb086f9492ff72998701222c392005ec
David Wilkie <dwilkie@gmail.com>
2014-02-16 18:01:29 +0700

    add flash message and simple styling to demo app

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,4 +2,9 @@ class ChargesController < ApplicationController
   def new
     @charge = Charge.new
   end
+
+  def create
+    @charge = Charge.new(params[:charge])
+    redirect_to root_path, :notice => "Successfully Created Charge"
+  end
 end
